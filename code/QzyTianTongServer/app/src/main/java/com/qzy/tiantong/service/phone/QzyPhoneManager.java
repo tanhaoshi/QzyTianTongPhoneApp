be7f1a64078ff35d644c8f84f669b593a1acf374
zhangyangjun@qq.com <zhangyangjun@qq.com>
2018-08-08 17:12:27 +0800

    加入电话信号强度监听

--- code/QzyTianTongServer/app/src/main/java/com/qzy/tiantong/service/phone/QzyPhoneManager.java
+++ code/QzyTianTongServer/app/src/main/java/com/qzy/tiantong/service/phone/QzyPhoneManager.java
@@ -3,8 +3,11 @@ package com.qzy.tiantong.service.phone;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.telephony.PhoneStateListener;
+import android.telephony.SignalStrength;
 import android.telephony.TelephonyManager;
 
 import com.qzy.tiantong.service.service.ITianTongServer;
@@ -16,6 +19,12 @@ import com.qzy.utils.LogUtils;
  */
 
 public class QzyPhoneManager {
+
+    private static final int NETWORKTYPE_WIFI = 0;
+    private static final int NETWORKTYPE_4G = 1;
+    private static final int NETWORKTYPE_2G = 2;
+    private static final int NETWORKTYPE_NONE = 3;
+
 
     private Context mContext;
     private ITianTongServer mServer;
@@ -33,6 +42,7 @@ public class QzyPhoneManager {
     private void setPhoneListener(){
         TelephonyManager tManager = (TelephonyManager)mContext.getSystemService(Service.TELEPHONY_SERVICE);
         tManager.listen(new MyTelephoneListener(), PhoneStateListener.LISTEN_CALL_STATE);
+        tManager.listen(new PhoneSignalStatListener(),PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
     }
 
     public class MyTelephoneListener extends PhoneStateListener {
@@ -80,5 +90,66 @@ public class QzyPhoneManager {
     public void hangupPhone() {
 
     }
+
+    private class PhoneSignalStatListener extends PhoneStateListener {
+        //获取信号强度
+
+        @Override
+        public void onSignalStrengthsChanged(SignalStrength signalStrength) {
+            super.onSignalStrengthsChanged(signalStrength);
+            //获取网络信号强度
+            //获取0-4的5种信号级别，越大信号越好,但是api23开始才能用
+            //            int level = signalStrength.getLevel();
+            int gsmSignalStrength = signalStrength.getGsmSignalStrength();
+            //获取网络类型
+            int netWorkType = getNetWorkType(mContext);
+            switch (netWorkType) {
+                case NETWORKTYPE_WIFI:
+                    LogUtils.e("network type wifi,signaleS：" + gsmSignalStrength);
+                    break;
+                case NETWORKTYPE_2G:
+                    LogUtils.e("network type 2G,signaleS：" + gsmSignalStrength);
+                    break;
+                case NETWORKTYPE_4G:
+                    LogUtils.e("network type 4G,signaleS：" + gsmSignalStrength);
+                    break;
+                case NETWORKTYPE_NONE:
+                    LogUtils.e("network type none,signaleS：" + gsmSignalStrength);
+                    break;
+                case -1:
+                    LogUtils.e("network type -1,signaleS：" + gsmSignalStrength);
+                    break;
+            }
+        }
+    }
+
+
+    public static int getNetWorkType(Context context) {
+        int mNetWorkType = -1;
+        ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            String type = networkInfo.getTypeName();
+            if (type.equalsIgnoreCase("WIFI")) {
+                mNetWorkType = NETWORKTYPE_WIFI;
+            } else if (type.equalsIgnoreCase("MOBILE")) {
+                return isFastMobileNetwork(context) ? NETWORKTYPE_4G : NETWORKTYPE_2G;
+            }
+        } else {
+            mNetWorkType = NETWORKTYPE_NONE;//没有网络
+        }
+        return mNetWorkType;
+    }
+
+    /**判断网络类型*/
+    private static boolean isFastMobileNetwork(Context context) {
+        TelephonyManager telephonyManager = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+        if (telephonyManager.getNetworkType() == TelephonyManager.NETWORK_TYPE_LTE) {
+            //这里只简单区分两种类型网络，认为4G网络为快速，但最终还需要参考信号值
+            return true;
+        }
+        return false;
+    }
+
 
 }
