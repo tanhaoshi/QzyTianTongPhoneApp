d40a103980e8c5357783acb86deee9e280426c2c
zhangyangjun <zhangyangjun@qq.com>
2018-12-08 11:31:02 +0800

    加入恢复wifi密码同时恢复出厂设置

--- code/QzyTianTongServer/appUpdate/src/main/java/com/qzy/tiantong/service/update/TiantongWifiManager.java
+++ code/QzyTianTongServer/appUpdate/src/main/java/com/qzy/tiantong/service/update/TiantongWifiManager.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Handler;
 import android.text.TextUtils;
 
 import com.qzy.tiantong.lib.utils.LogUtils;
@@ -46,12 +47,17 @@ public class TiantongWifiManager {
      */
     private BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
-        public void onReceive(Context context, Intent intent) {
+        public void onReceive(final Context context, Intent intent) {
             String action = intent.getAction();
             LogUtils.d("action = " + action);
             if (action.equals("com.qzy.tt.ACTION_RECOVERY_WIFI")) {
                 setWifiPasswd(QzyTtContants.WIFI_PASSWD);
-
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                     doMasterClear(context);
+                    }
+                },3000);
             } else if (action.equals("com.qzy.tt.ACTION_CHANGE_WIFI")) {
                 String passwd = intent.getStringExtra("passwd");
                 setWifiPasswd(passwd);
@@ -68,6 +74,20 @@ public class TiantongWifiManager {
         WifiUtils.setWifiPasswdToSharedpref(mContext, passwd);
         WifiUtils.setWifiApEnabled(mContext, WifiUtils.getSsidName(), passwd, false);
         WifiUtils.setWifiApEnabled(mContext, WifiUtils.getSsidName(), passwd, true);
+    }
+
+    /**
+     * 恢复出厂设置
+     *
+     * @param context
+     */
+    public void doMasterClear(Context context) {
+        Intent intent = new Intent("android.intent.action.MASTER_CLEAR");
+        intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
+        intent.putExtra("android.intent.extra.REASON", "MasterClearConfirm");
+        intent.putExtra("android.intent.extra.WIPE_EXTERNAL_STORAGE", false);
+        context.sendBroadcast(intent);
+        // Intent handling is asynchronous -- assume it will happen soon.
     }
 
     public void free() {
