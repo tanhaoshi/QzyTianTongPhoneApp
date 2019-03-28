7b9d2a5fcd693c0c4a25d1580691146b5b6b420a
zhangyangjun <zhangyangjun@qq.com>
2019-03-28 21:14:57 +0800

    修改获取 wifi 名称去掉测试字符串

--- code/QzyTianTongServer/appUpdate/src/main/java/com/qzy/tiantong/service/contants/QzyTtContants.java
+++ code/QzyTianTongServer/appUpdate/src/main/java/com/qzy/tiantong/service/contants/QzyTtContants.java
@@ -2,7 +2,7 @@ package com.qzy.tiantong.service.contants;
 
 public class QzyTtContants {
 
-    public static final String WIFI_SSID = "HWA_6100G_test_new";
+    public static final String WIFI_SSID = "HWA_6100G_";
 
     public static final String WIFI_PASSWD = "12345678";
 
